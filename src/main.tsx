c6ffa4613276dcf889882ddf76dce4ee713a46d9
Nearh777 <nearh777@gmail.com>
2025-04-27 21:18:55 +0300

    hw3.1 part2

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import { RouterProvider} from "react-router-dom";
-import {routes} from "./components/routes/routes.tsx";
+import {BrowserRouter, Route, Routes} from "react-router";
+import {UsersComponent} from "./components/users-component/UsersComponent.tsx";
+import { PostsComponent } from './components/posts-component/PostsComponent.tsx';
+import {CommentsComponent} from "./components/comments-component/CommentsComponent.tsx";
+import {ProductsComponent} from "./components/products-component/ProductsComponent.tsx";
+import App from './App.tsx';
+// import { RouterProvider} from "react-router-dom";
+// import {routes} from "./components/routes/routes.tsx";
 
 
 
+
+// createRoot(document.getElementById('root')!)
+//     .render( <RouterProvider router={routes} /> )
 
 createRoot(document.getElementById('root')!)
-    .render( <RouterProvider router={routes} /> )
+    .render(<BrowserRouter>
+        <Routes>
+
+            <Route path={'/'} element={<App/>}>
+                <Route path={'users'} element={<UsersComponent/>}/>
+                <Route path={'posts'} element={<PostsComponent/>}/>
+                <Route path={'comments'} element={<CommentsComponent/>}/>
+                <Route path={'products'} element={<ProductsComponent/>}/>
+
+            </Route>
+
+        </Routes>
+
+
+    </BrowserRouter>);
